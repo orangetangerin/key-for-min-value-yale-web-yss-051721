393452d780c87396d864cd11a8d47fd82d3a609c
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-03 03:46:39 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_val = nil
+  min_key = nil
+  name_hash.each do |key, value|
+    if min_val == nil
+      min_val = value
+      min_key = key
+    elsif min_val > value
+      min_val = value
+      min_key = key
+    end
+  end
+  return min_key
 end
